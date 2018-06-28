1d7e8145cf653c076b797d63b0da8be8802cdb6b
William Feng <178894043@qq.com>
2018-06-28 17:14:13 +0800

    base function done

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # SimpleFileServer
+
+文件管理服务器
+
+## 使用
+
+### 参数
+
+- `-port`
+    - 端口
+    - 默认9410
+- `-rootDir`  
+    - 根目录路径
+    - 为了安全，不填会直接报错
+
+### 简单版本
+
+使用最新的release包执行运行就好
+
+### 复杂版本
+
+- 在拉下来的代码目录下 `go build` 可以得到SimpleFileServer的可执行文件
+- 以windows为例，在当前目录下`.\SimpleFileServer.exe -port=9410 -rootDir="F:\FromGithub"`
+- 浏览 `http://127.0.0.1:9410`即可
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"flag"
+	"log"
+	"os"
+)
+
+type Config struct {
+	RootDir string
+	Port    string
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello From PC :)"))
+}
+
+func exists(path string) bool {
+	_, err := os.Stat(path)    //os.Stat获取文件信息
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
+}
+
+func main() {
+	fPort := flag.String("port", "9410", "port for server")
+	fRootDir := flag.String("rootDir", "", "root directory for server")
+	flag.Parse()
+
+	if *fRootDir == "" {
+		log.Fatalf("Have not set root dir!")
+		return
+	}
+	if !exists(*fRootDir) {
+		log.Fatalf("Dir not exists")
+		return
+	}
+	log.Println(*fRootDir)
+
+	http.HandleFunc("/", indexHandler)
+	http.Handle("/view/", http.StripPrefix("/view", http.FileServer(http.Dir(*fRootDir))))
+	http.ListenAndServe("127.0.0.1:" + *fPort, nil)
+}
